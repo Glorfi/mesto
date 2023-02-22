39064f7736095c38d6e6c4c39d212fd15d01c1fa
Konstantin Bobkov <mrglorf@gmail.com>
2023-02-22 08:39:03 +0400

    feat: enable like buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
+
 let ProfilePopup = document.querySelector(".popup");
 let EditProfileButton = document.querySelector(".profile__edit-button");
 let CloseProfileButton = document.querySelector(".popup__button-close");
 let UserName = document.querySelector(".profile__name");
 let UserDescription = document.querySelector(".profile__description");
 let FormElement = document.querySelector(".popup__form");
-
 let NameInput = document.querySelector(".popup__input_name");
 let AboutInput = document.querySelector(".popup__input_about");
+
+const LikeButton = document.querySelectorAll(".card__like-button");
 
 function openPopUp(popup) {
   NameInput.value = UserName.innerText;
@@ -16,12 +18,20 @@ function openPopUp(popup) {
 function closePopUp(popup) {
   popup.classList.remove("popup_opened");
 }
+function toggleLike(like) {
+  like.classList.toggle("card__like-button_active");
+}
 
 EditProfileButton.addEventListener("click", function () {
   openPopUp(ProfilePopup);
 });
 CloseProfileButton.addEventListener("click", function () {
   closePopUp(ProfilePopup);
+});
+LikeButton.forEach((item) => {
+  item.addEventListener("click", function () {
+    toggleLike(item);
+  });
 });
 
 function formSubmitHandler(evt) {
